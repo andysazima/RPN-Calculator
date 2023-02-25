2119de5fa2064c7edc980d9367d5dead74accbce
andysazima <94436038+andysazima@users.noreply.github.com>
2023-02-24 23:14:18 -0800

    added textual and rich

--- main.py
+++ main.py
@@ -4,26 +4,25 @@ from decimal import Decimal
 
 def main():
     rpn_stack = Stack()
-    while True:
-        expression = input("Expression: ")
-        expression = expression.strip().split(sep=" ")
-        expression = [x for x in expression if x]
+    expression = input("Expression: ")
+    expression = expression.strip().split(sep=" ")
+    expression = [x for x in expression if x]
 
-        for x in expression:
-            if x == "+":
-                n1 = Decimal(rpn_stack.pop())
-                n2 = Decimal(rpn_stack.pop())
-                n3 = n2 + n1
-                rpn_stack.push(n3)
-            elif x == "-":
-                n1 = Decimal(rpn_stack.pop())
-                n2 = Decimal(rpn_stack.pop())
-                n3 = n2 - n1
-                rpn_stack.push(n3)
-            else:
-                rpn_stack.push(x)
+    for x in expression:
+        if x == "+":
+            n1 = Decimal(rpn_stack.pop())
+            n2 = Decimal(rpn_stack.pop())
+            n3 = n2 + n1
+            rpn_stack.push(n3)
+        elif x == "-":
+            n1 = Decimal(rpn_stack.pop())
+            n2 = Decimal(rpn_stack.pop())
+            n3 = n2 - n1
+            rpn_stack.push(n3)
+        else:
+            rpn_stack.push(x)
 
-        print(f"\n{rpn_stack.pop()}")
+    print(f"{rpn_stack.pop()}")
 
 
 if __name__ == "__main__":
